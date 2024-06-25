58536ee8340a2c31ed2ff8e1ed00f15652db01ef
Daniel Hongel <fatherofdharma@gmail.com>
2024-06-25 12:40:52 -0700

    Start lessons up to Lesson 8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { purple } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -13,7 +32,8 @@ function App() {
           <Create />
         </Route>
       </Switch>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
