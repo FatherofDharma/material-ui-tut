fa6981b7112d7113012c99516b192f30a764d83e
Daniel Hongel <fatherofdharma@gmail.com>
2024-08-02 14:07:27 -0700

    complete tutorial code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
-import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import { purple } from '@material-ui/core/colors';
+import Layout from './components/Layout';
 
 const theme = createTheme({
   palette: {
@@ -18,20 +19,22 @@ const theme = createTheme({
     fontWeightMedium: 600,
     fontWeightBold: 700
   }
-})
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { Container, Grid, Paper } from '@material-ui/core';
+import NoteCard from '../components/NoteCard';
+import Masonry from 'react-masonry-css';
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -12,29 +14,37 @@ export default function Notes() {
       .then(data => setNotes(data))
   }, [])
 
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE'
+    })
+
+    //update db with filtered list of notes to update for deleted note object
+    const newNotes = notes.filter(note => note.id != id)
+    setNotes(newNotes)
+  }
+
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
-      {/* <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
-      </Grid> */}
-      <Grid container>
-      {notes.map(note => (
-        <Grid item key={note.id} xs={12} md={6} lg={4}>
-          <Paper>{note.title}</Paper>
-        </Grid>
-      ))}
-      </Grid>
+
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {notes.map(note => (
+            <div key={note.id} >
+              <NoteCard note={note} handleDelete={handleDelete}/>
+            </div>
+        ))}
+      </Masonry>
+
     </Container>
   )
 }
